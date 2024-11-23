7adba58b82efe96aa78b8d58463a7bbccbadf9d7
Acorjuela <105765061+Acorjuela@users.noreply.github.com>
2024-11-23 10:21:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
  
  // Temporizador
  const countdown = () => {
-   const launchDate = new Date("Dec 28, 2024 02:10:00").getTime(); // Fecha límite
+   const launchDate = new Date("Dec 8, 2024 02:10:00").getTime(); // Fecha límite
    const now = new Date().getTime();
    const timeLeft = launchDate - now;
  
